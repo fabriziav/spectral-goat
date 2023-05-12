380a159fc9c758063c82721815f11d91ea29fb17
fabriziav <75419607+fabriziav@users.noreply.github.com>
2023-05-12 12:40:08 +0200

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -26,4 +26,27 @@ resource "aws_key_pair" "ssh" {
   key_name = "provkey"
   public_key = file("~/testec2.pub")
 }
+resource "aws_security_group" "prov_fw" {
+  name = "prov_fw"
 
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
